7881090eb5230fd2f6339fd09405bab51f485b0b
Elie Prudhomme <eprudhomme@gmail.com>
2018-10-17 00:30:48 +0200

    Add npm release run script

---
+++ bin/release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git stash save 'Before release'
+rm -rf build/
+npm run build
+npm test
+if [ $? == 0 ]; then
+    git tag -a v$npm_package_version -m "Version $npm_package_version"
+    git push --tags
+    npm publish . --access public
+else
+    echo ' >> Release aborted << '
+fi
+git stash pop
+echo ' >> Released << '
+
